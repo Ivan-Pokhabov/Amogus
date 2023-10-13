b035bd867e5c34c25a313b8ff17ad0f0c6cd96eb
Ivan-Pokhabov <vanek3372@gmail.com>
2023-10-13 14:41:13 +0300

    Add some changes

--- queueMain/queue.c
+++ queueMain/queue.c
@@ -5,12 +5,27 @@
 
 typedef struct
 {
-	int* head;
-	int* back;
+	QueueElement* head;
+	QueueElement* back;
 } Queue;
 
 typedef struct {
 	int value;
-	int* next;
-	int* prev;
-};
+	struct QueueElement* next;
+} QueueElement;
+
+void enqueue(Queue queue, const int value)
+{
+	QueueElement* newElem = malloc(sizeof(QueueElement));
+	newElem->value = value;
+	if (!isEmpty(queue))
+	{
+		queue.back->next = newElem;
+	}
+	queue.back = newElem;
+}
+
+void dequeue(Queue queue)
+{
+	
+}
